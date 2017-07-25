2a3152beb051f875e7e5329c0052e6fc31d9699d
ashifer <ashwine@freshdesk.com>
2017-07-25 17:32:50 +0530

    Update fixture.js

--- fixture.js
+++ fixture.js
@@ -5,10 +5,10 @@ var sqlFixtures = require('sql-fixtures');
 var dbConfig = {
 	client: 'mysql',
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password:process.env.DB_PASS,
-    database: 'meeting',
+    host: '127.0.0.1',
+    user: 'root',
+    password:'root',
+    database: 'circle_test'
   }
 };
  
