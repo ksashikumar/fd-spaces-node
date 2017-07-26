98a4182f71863bfe816e66cf7559a382fd2de6f8
hariniks10 <hariniks@freshdesk.com>
2017-07-26 13:28:40 +0530

    Update drop_table.js

--- test/unit/drop_table.js
+++ test/unit/drop_table.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "gayu1997",
-  database: "mroomtest"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.TEST_DB_NAME
 });
 
 con.connect(function(err) {
