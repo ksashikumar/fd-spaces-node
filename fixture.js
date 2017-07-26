2f92f74296f064f82dcc9dc2e9e290652e68fd00
gayathri2601 <sgayathri@freshdesk.com>
2017-07-26 15:58:22 +0530

    Update fixture.js

--- fixture.js
+++ fixture.js
@@ -8,7 +8,7 @@ var dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password:process.env.DB_PASS,
-    database: 'process.env.DB_NAME',
+    database:process.env.DB_NAME,
   }
 };
  
