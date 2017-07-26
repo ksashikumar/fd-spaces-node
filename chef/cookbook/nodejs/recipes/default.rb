c3659506fe712edca05eb830a19fab38591d0a0a
user <user@FDLMC333.local>
2017-07-26 07:09:22 +0530

    Chef-solo added

---
+++ chef/cookbook/nodejs/recipes/default.rb
@@ -0,0 +1,8 @@
+package "nodejs" do
+	action :install
+end
+
+execute 'install grunt' do
+  command "npm install -g grunt-cli"
+  not_if "npm --no-color -g ls 'grunt-cli' 2> /dev/null | grep 'grunt-cli'"
+end
---
+++ solo.rb
@@ -0,0 +1,3 @@
+file_cache_path "/Users/user/chef"
+cookbook_path "/Users/user/chef/cookbook"
+json_attribs "/Users/user/chef/node.json"
